3d52f617d705b5fcc0f1008332ec3e6c15d31983
Vijayasaran V <117149477+Vijayasaran-VJVS@users.noreply.github.com>
2023-10-03 13:51:21 +0530

    Add files via upload

---
+++ Weather/script.js
@@ -0,0 +1,56 @@
+const inputBox = document.querySelector('.input-box');
+const searchbtn = document.getElementById('searchbtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const windspeed = document.getElementById('wind-speed');
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+async function checkweather(city) {
+    const api_key = 'dd2611bd1efae5ec51c6b2ba4d4baf24';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+    if(weather_data.cod === '404'){
+        location_not_found.style.display = 'flex';
+        weather_body.style.display = 'none';
+        console.log('error');
+        return;
+    }
+
+    location_not_found.style.display = 'none';
+    weather_body.style.display = 'flex';
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    windspeed.innerHTML = `${weather_data.wind.speed}km/Hr`;
+
+    switch (weather_data.weather[0].main) {
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+    }
+
+    console.log(weather_data);
+
+
+}
+
+searchbtn.addEventListener('click', () => {
+    checkweather(inputBox.value);
+});
